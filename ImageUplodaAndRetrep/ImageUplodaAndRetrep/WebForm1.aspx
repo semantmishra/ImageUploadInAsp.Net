d9c75cf5bd4dd5ab7e78826014f7394bd232b6fb
MultiStar Semant.Mishra <49635322+semantmishra@users.noreply.github.com>
2019-07-13 13:22:52 +0530

    Add files via upload

---
+++ ImageUplodaAndRetrep/ImageUplodaAndRetrep/WebForm1.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ImageUplodaAndRetrep.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    </head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table border="0" cellpadding="2" width="500px">
+            <tr>
+                <td >
+                    Upload File</td>
+                <td >
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td >
+                    &nbsp;</td>
+                <td >
+                    <asp:Button ID="Button1" runat="server" onclick="Button1_Click" Text="Upload" />
+                    <asp:Label ID="Label1" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td >
+                    &nbsp;</td>
+                <td >
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="2" >
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+                        ShowHeader="False">
+                    <Columns>
+                        <asp:TemplateField>
+                            <ItemTemplate>
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <asp:Label ID="Label2" runat="server" Text='<%#Eval("id") %>'>"></asp:Label></td>
+                                        <td>
+                                            <asp:Image ID="Image1" Height="150" Width="150" ImageUrl='<%#Eval("Image") %>' runat="server" /></td>
+                                    </tr>
+                                </table>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>   
+    </div>
+    </form>
+</body>
+</html>
---
+++ ImageUplodaAndRetrep/ImageUplodaAndRetrep/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Data;
+
+namespace ImageUplodaAndRetrep
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadGridView();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String  connString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            String path = Server.MapPath("Images/");
+            if (FileUpload1.HasFile)
+            {
+                String name = Path.GetFileName(FileUpload1.FileName);
+                String Exteson = Path.GetExtension(FileUpload1.FileName);
+                HttpPostedFile postfile = FileUpload1.PostedFile;
+                int len = postfile.ContentLength;
+               
+                if (Exteson.ToLower() == ".jpg" || Exteson.ToLower() == ".jpeg" || Exteson.ToLower() == ".png")
+                {
+                    if (len <= 1000000)
+                    {
+                        FileUpload1.SaveAs(path + name);
+                        string na = "Images/" + name;
+                        string Sq = "insert into ImageTbl(Image) values(@img) ";
+                        SqlCommand cmd = new SqlCommand(Sq, conn);
+                        conn.Open();
+                        cmd.Parameters.AddWithValue("@img", na);
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                        Label1.Text = "Upload Success";
+                        Label1.ForeColor = System.Drawing.Color.Blue;
+                        LoadGridView();
+                    }
+                    else 
+                    {
+                        Label1.Text = "File Size Vary Large!!!";
+                        Label1.ForeColor = System.Drawing.Color.Red;
+                    }
+
+                }
+                else
+                {
+                    Label1.Text = "Plese Choose Correct Image!!!";
+                    Label1.ForeColor = System.Drawing.Color.Red;
+                }
+
+            }
+            else
+            {
+                Label1.Text = "Plese Upload an Image!!!";
+                Label1.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        void LoadGridView() 
+        {
+            String connString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            SqlDataAdapter d = new SqlDataAdapter("Select * from ImageTbl", conn);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+    }
+}
